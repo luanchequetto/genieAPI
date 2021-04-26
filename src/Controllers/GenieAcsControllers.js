29f5cf7b924030becd6781fa09ea0e853da2576e
Luan Chequetto <luan.gaertner900@gmail.com>
2021-04-26 15:37:36 -0300

    create variables for public repo

--- src/Controllers/GenieAcsControllers.js
+++ src/Controllers/GenieAcsControllers.js
@@ -1,3 +1,5 @@
+const mongodbURL = 'mongodb://localhost/genieacs'; // Endereço do mongoDB
+const genieURL = 'http://localhost:7575'; // Endereço do servidor Genie + porta de acesso
 const mongoose = require('mongoose');
 const dataSchema = new mongoose.Schema({});
 const dataModel = mongoose.model('device', dataSchema, 'devices');
@@ -10,7 +12,7 @@ export default class GenieAcsControllers {
     async setTag(req, res) {
         const wanIP = req.query.wanIP
         const tag = req.query.tag
-        mongoose.connect('mongodb://acs.unifique.com.br/genieacs', { useNewUrlParser: true, useUnifiedTopology: true })
+        mongoose.connect(mongodbURL, { useNewUrlParser: true, useUnifiedTopology: true })
             .then(
                 () => {
                     dataModel.find({
@@ -22,16 +24,13 @@ export default class GenieAcsControllers {
                             "-" + mongoResponse._deviceId._SerialNumber);
                             console.log(response)
 
-                            axios.post(`http://acs.unifique.com.br:7557/devices/${response}/tags/${tag}`)
+                            axios.post(`${genieURL}${response}/tags/${tag}`)
                             .then(resp=>{
                                 res.send('Bloqueio realizado com sucesso')
                             })
                             .catch(error=>{
                                 res.send('Houve uma falha ao realizar o bloqueio')
-                            })
-
-
-                        
+                            })                       
                     })
 
                 },
@@ -46,7 +45,7 @@ export default class GenieAcsControllers {
     async unsetTag(req, res) {
         const wanIP = req.query.wanIP
         const tag = req.query.tag
-        mongoose.connect('mongodb://acs.unifique.com.br/genieacs', { useNewUrlParser: true, useUnifiedTopology: true })
+        mongoose.connect(mongodbURL, { useNewUrlParser: true, useUnifiedTopology: true })
             .then(
                 () => {
                     dataModel.find({
@@ -57,7 +56,7 @@ export default class GenieAcsControllers {
                             "-" + mongoResponse._deviceId._ProductClass +
                             "-" + mongoResponse._deviceId._SerialNumber);
 
-                            axios.delete(`http://acs.unifique.com.br:7557/devices/${response}/tags/${tag}`)
+                            axios.delete(`${genieURL}${response}/tags/${tag}`)
                             .then(resp=>{
                                 res.send('Bloqueio removido com sucesso!')
                             })
